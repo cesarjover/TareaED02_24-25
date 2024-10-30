6f987506433e6c4e81fa8d7c02bcd9cac1e7ecb8
ROcpifpbajoaragon <157353847+ROcpifpbajoaragon@users.noreply.github.com>
2024-10-30 07:29:46 +0100

    corrección

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
-
-public class Main {
-
+public class Main
+extends JFrame {
+public Main() {
+JLabel lblSaludo = new JLabel("Hola Mundo. Creando mi primer ejemplo");
+add(lblSaludo);
+this.setSize(400,200);
+this.setTitle("JFrame");
+this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+this.setVisible(true);
+}
+public static void main(String[] args) {
+Main main = new Main();
+}
 }
